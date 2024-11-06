89509e1fcf3201b7b5a5bba214ae2efd15b95be2
Dmitriy Goryachev <dmitriigoryachev58@gmail.com>
2024-11-06 09:10:37 +0300

    loaded standard ranger conf to make it usable

--- backup/home/citysexx/.config/qutebrowser/config.py
+++ backup/home/citysexx/.config/qutebrowser/config.py
@@ -5,7 +5,7 @@ config.load_autoconfig()
 # set the flavor you'd like to use
 # valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
 # last argument (optional, default is False): enable the plain look for the menu rows
-catppuccin.setup(c, 'mocha', True)
+catppuccin.setup(c, 'frappe', True)
 
 
 config.set("colors.webpage.darkmode.enabled", True)
